87c83b2f5746392fe70771ed148ba8d315c87407
morozzov <romm858@mail.ru>
2022-03-15 22:29:43 +0300

    Done/unDune added

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ const pool = new Pool({
 
 const urlencodedParser = express.urlencoded({extended: false});
 
-var dataJ
+app.use('*/css',express.static('public/css'));
+app.use('*/fonts',express.static('public/fonts'));
 
 app.get("/getDone", (req, res) => {
-
-pool.query(
+    pool.query(
      "select * from tasks where is_done = true;",
      [],
      (error, results) => {
@@ -26,7 +26,7 @@ pool.query(
        } 
        res.status(200).json(results.rows);
      }
-   );
+    );
 });
 
 app.get("/get", (req, res) => {
@@ -44,21 +44,33 @@ pool.query(
 app.get("/", function (request, response) {
     response.sendFile(__dirname + "/index.html");
 });
-app.use('*/css',express.static('public/css'));
-app.use('*/fonts',express.static('public/fonts'));
+
 
 app.get("/done", function (request, response) {
     response.sendFile(__dirname + "/public/done.html");
 });
+
 app.get("/about", function (request, response) {
     response.sendFile(__dirname + "/public/about.html");
 });
+
 app.post("/", urlencodedParser, function (request, response) {
     if(!request.body) return response.sendStatus(400);
     console.log(request.body);
-    // response.send(`${request.body.newTitle} - ${request.body.newBody } - ${request.body.newColor}`);
     newTask(request.body.newTitle, request.body.newBody, request.body.newColor)
     response.redirect("/");
+});
+
+app.post("/setdone/:id", urlencodedParser, function (request, response) {
+    if(!request.params.id) return response.sendStatus(400);
+    setDone(request.params.id)
+    response.redirect("/");
+});
+
+app.post("/unsetdone/:id", urlencodedParser, function (request, response) {
+    if(!request.params.id) return response.sendStatus(400);
+    unsetDone(request.params.id)
+    response.redirect("/done");
 });
    
 app.listen(3000, ()=>console.log("Server started..."));
@@ -77,20 +89,28 @@ function newTask(newTitle, newBody, newColor) {
     });
 }
 
-function getTasks() {
-
+function setDone(id) {
     var pgp = require("pg-promise")(/*options*/);
     var db = pgp("postgres://postgres:123@localhost:5432/postgres");
-    var res = null;
-    db.any(`select * from tasks where is_done = false;`)
-    .then(function (data) {
-        res = data
-        console.log("SUCCESS: ", data);
-        console.log("SUS: ", res);
-        return res
+    
+    db.one(`update tasks set is_done = true where id = ${id};`)
+    .then(function () {
+        console.log("SUCCESS");
     })
     .catch(function (error) {
         console.log("ERROR:", error);
     });
-    return res
+}
+
+function unsetDone(id) {
+    var pgp = require("pg-promise")(/*options*/);
+    var db = pgp("postgres://postgres:123@localhost:5432/postgres");
+    
+    db.one(`update tasks set is_done = false where id = ${id};`)
+    .then(function () {
+        console.log("SUCCESS");
+    })
+    .catch(function (error) {
+        console.log("ERROR:", error);
+    });
 }
